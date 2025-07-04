07e91a1be2d62a26045d375cc66894eccd67236c
soo <dlgustn96@gmail.com>
2025-07-05 01:30:39 +0900

    refactor: PrismaModule에 트랜잭션 관련 설정 추가

--- src/infrastructure/prisma/prisma.module.ts
+++ src/infrastructure/prisma/prisma.module.ts
@@ -1,9 +1,28 @@
-import { Global, Module } from '@nestjs/common';
+import { Global, Inject, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import {
+  TRANSACTION_MANAGER,
+  TransactionManager,
+} from 'src/application/common/transaction-manager';
+import { registerGlobalTransactionManager } from 'src/interfaces/common/decorators/transactional.decorator';
+import { PrismaTransactionManager } from './prisma.transaction-manager';
 
 @Global()
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: TRANSACTION_MANAGER,
+      useClass: PrismaTransactionManager,
+    },
+  ],
+  exports: [PrismaService, TRANSACTION_MANAGER],
 })
-export class PrismaModule {}
+export class PrismaModule {
+  constructor(
+    @Inject(TRANSACTION_MANAGER)
+    private readonly transactionManager: TransactionManager
+  ) {
+    registerGlobalTransactionManager(this.transactionManager);
+  }
+}
